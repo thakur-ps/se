87aa3eb917528b0e11472aaebad83a2923233dbf
Pushpendra Singh thakur <thakurpsr@gmail.com>
2020-07-07 16:02:13 +0530

    UPDATE Response class status now reflects HTTP_STATUS

--- Response.class.php
+++ Response.class.php
@@ -9,8 +9,15 @@
 	
 	class Response{
 		
-		const STATUS_OK = 1;
-		const STATUS_ERROR = 0;
+		const OK_OK = 'OK';
+		const OK_CREATED = 'CREATED';
+		const OK_ACCEPTED = 'ACCEPTED';
+		const OK_NO_CONTENT = 'NO_CONTENT';
+		
+		const ERROR_BAD_REQUEST = 'BAD_REQUEST';
+		const ERROR_NOT_FOUND = 'NOT_FOUND';
+		const ERROR_CONFLICT = 'CONFLICT';
+		const ERROR_ERROR = 'INTERNAL_ERROR';
 		
 		protected $started = false;
 		protected $ended = false;
@@ -29,6 +36,35 @@
 				return;
 			}
 			
+			switch( $status ){
+				case self::OK_OK:
+					header( 'HTTP/1.1 200 OK' );
+					break;
+				case self::OK_CREATED:
+					header( 'HTTP/1.1 201 Created' );
+					break;
+				case self::OK_ACCEPTED:
+					header( 'HTTP/1.1 202 Accepted' );
+					break;
+				case self::OK_NO_CONTENT:
+					header( 'HTTP/1.1 204 No Content' );
+					break;
+					
+				case self::ERROR_BAD_REQUEST:
+					header( 'HTTP/1.1 400 Bad Request' );
+					break;
+				case self::ERROR_NOT_FOUND:
+					header( 'HTTP/1.1 404 Not Found' );
+					break;
+				case self::ERROR_CONFLICT:
+					header( 'HTTP/1.1 409 Conflict' );
+					break;
+				case self::ERROR_ERROR:
+					header( 'HTTP/1.1 500 Internal Server Error' );
+					break;
+				default:
+					throw new \Exception( 'UNKNOWN_RESPONSE_HEADER' );
+			}
 			/* Enable Cross Origin Resource Sharing */
 			header( 'Access-Control-Allow-Origin: '.$this->corsDomain );
 			header( 'Access-Control-Allow-Credentials: true' );
@@ -79,13 +115,31 @@
 			echo json_encode( $data );
 		}
 		
-		function sendOk( $message="" ){
-			$this->start( 1, array( '_mes'=>$message ) );
+		function sendOk( $status, $message="" ){
+			switch( $status ){
+				case self::OK_OK:
+				case self::OK_CREATED:
+				case self::OK_ACCEPTED:
+				case self::OK_NO_CONTENT:
+					break;
+				default:
+					throw new \Exception( 'BAD_OK_STATUS' );
+			}
+			$this->start( $status, array( '_mes'=>$message ) );
 			$this->end();
 		}
 		
-		function sendError( $message, $field="" ){
-			$this->start( 0, array( 
+		function sendError( $status, $message, $field="" ){
+			switch( $status ){
+				case self::ERROR_BAD_REQUEST:
+				case self::ERROR_NOT_FOUND:
+				case self::ERROR_CONFLICT:
+				case self::ERROR_ERROR:
+					break;
+				default:
+					throw new \Exception( 'BAD_ERROR_STATUS' );
+			}
+			$this->start( $status, array( 
 								'_mes'=>$message,
 								'_fie'=>$field 
 								) );
